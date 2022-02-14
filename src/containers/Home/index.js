b2c3829069759361ef537387180f01758caae370
DHC961114 <99641769+DHC961114@users.noreply.github.com>
2022-02-14 16:16:45 +0800

    Revert "feat: 首页增加Olympic 冰墩墩🐼页面入口."
    This reverts commit 73b741ce76aa781d950a05e8a056d2081da9fd8e.

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -17,7 +17,6 @@ import carImage from './images/car.png';
 import developingImage from './images/developing.png';
 import segmentFaultImage from './images/segmengfault.png';
 import humanImage from './images/human.png';
-import olympicImage from './images/olympic.png';
 import './index.css';
 
 const Item = styled(Paper)(({ theme }) => ({
@@ -40,13 +39,6 @@ const workList = [
     title: '脸书Meta元宇宙Logo',
     description: '🪐 Three.js + Blender 实现炫酷的Facebook元宇宙Logo.',
     image: metaImage,
-    three: true
-  },
-  {
-    link: '#/olympic',
-    title: '2022冬奥会3D趣味页面',
-    description: '🐼一只萌萌的冰墩墩送给大家！',
-    image: olympicImage,
     three: true
   },
   {
